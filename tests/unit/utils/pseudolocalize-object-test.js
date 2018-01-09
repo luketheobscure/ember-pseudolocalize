f51a368c4c66c732942ee119f001646092323e05
Luke Deniston <luke@agrian.com>
2018-01-09 12:34:59 -0800

    Fix tests.

--- tests/unit/utils/pseudolocalize-object-test.js
+++ tests/unit/utils/pseudolocalize-object-test.js
@@ -1,9 +1,13 @@
 import pseudolocalizeObject from 'dummy/utils/pseudolocalize-object';
 import { module, test } from 'qunit';
+import pseudoloc from 'pseudoloc';
 
 module('Unit | Utility | pseudolocalize object');
 
 test('it works', (assert) => {
+  pseudoloc.option.prepend = '[!!';
+  pseudoloc.option.append = '!!]';
+
   const result = pseudolocalizeObject({
     aString: 'a string',
     anObject: {
